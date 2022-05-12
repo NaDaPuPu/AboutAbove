02d19ba2e6d1f39b1b2e67cc5399ff269a08c557
NaDaPuPu <jun19998@gmail.com>
2022-05-12 18:44:07 +0900

    Feat: Main <-> Test 액티비티 전환 효과 추가
    전환 시 튕기는 효과 추가

--- app/src/main/java/com/pupu/aboutabove/MainActivity.kt
+++ app/src/main/java/com/pupu/aboutabove/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.pupu.aboutabove
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.pupu.aboutabove.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     private var mBinding: ActivityMainBinding? = null
     private val binding get() = mBinding!!
 
@@ -17,5 +19,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         randomTheme.aboveScript(this, binding.textViewMainScript, binding.imageViewMainMember)
+
+        binding.buttonMainTest.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v) {
+            binding.buttonMainTest -> {
+                val intent = Intent(this, TestActivity::class.java)
+                startActivity(intent)
+                overridePendingTransition(R.anim.slide_right_enter, R.anim.slide_right_exit)
+                finish()
+            }
+
+            binding.buttonMainMember -> {
+
+            }
+        }
     }
 }
--- app/src/main/java/com/pupu/aboutabove/TestActivity.kt
+++ app/src/main/java/com/pupu/aboutabove/TestActivity.kt
@@ -1,5 +1,6 @@
 package com.pupu.aboutabove
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,12 @@ class TestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        overridePendingTransition(R.anim.slide_left_enter, R.anim.slide_left_exit)
     }
 }
